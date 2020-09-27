4c8396ab3c19f6a06ed472d76f028057029fdf21
Sabrina Djeddi <66637870+sabdj2020@users.noreply.github.com>
2020-09-27 10:17:32 -0500

    sabrina last commit

--- mysite/checkers/views.py
+++ mysite/checkers/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .models import Adherent
+from operator import itemgetter
+
 
 # Create your views here.
+def homeview (request):
+    return render(request, 'checkers/home.html')
+
+def playerview (request):
+    return render(request, 'checkers/playerview.html')
+
+def signup (request):
+    if request.method=="POST":
+       nickname = request.POST['nickname']
+       email = request.POST['email']
+       password = request.POST['password']
+    #check if nickname doesn't exist, then create the new player
+    User.objects.create_user(username=nickname, password=password).save()
+    lo = len(User.objects.all())-1
+    Adherent(id = User.objects.all()[int(lo)].id, email=email).save()
+    messages.add_message(request, messages.ERROR,"Sign Up Successful")
+    return redirect('login')
+
+    # check if nickname exist
+    if User.objects.filter( username=nickname).exists():
+        messages.add_message(request, messages.ERROR,"this nickname is already in use")
+        return redirect('home')
+
+def loginp (request):
+    return render(request,'checkers/login.html')
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'checkers'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'checkersdb',
+        'USER':'root',
+        'PASSWORD':'root',
+        'PORT':'3306',
+        'HOST':'127.0.0.1',
     }
 }
 
@@ -118,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static']
