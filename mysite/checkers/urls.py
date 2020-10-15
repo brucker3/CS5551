6bceff53bddf1451e433cb0518f695486f1999be
Sabrina Djeddi <66637870+sabdj2020@users.noreply.github.com>
2020-10-15 13:18:13 -0500

    Update urls.py

--- mysite/checkers/urls.py
+++ mysite/checkers/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login/',loginview.as_view(), name='login'),
     path('logout/',logoutview.as_view(), name='logout'),
     path('game/', game.as_view(), name='game'),
-    path('rules', rulesview.as_view(), name='rules'),
-    path('player_stats', player_statsview.as_view(), name='player_stats'),
+    path('rules/', rulesview.as_view(), name='rules'),
+    path('player_stats/', player_statsview.as_view(), name='player_stats'),
 
 ]
