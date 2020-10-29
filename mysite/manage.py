a272f539a6c0d099f392fca621070c255d99f0a6
zealpatel1990 <zealpatel1990@gmail.com>
2020-10-29 15:35:47 -0500

    redis server will start automatically is docker is running

--- mysite/manage.py
+++ mysite/manage.py
@@ -5,18 +5,23 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+	"""Run administrative tasks."""
+	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+	
+	"""Start redis server using docker"""
+	print ("starting redis server by docker run -p 6379:6379 -d redis:2.8")
+	os.system("docker run -p 6379:6379 -d redis:2.8")
+
+	try:
+		from django.core.management import execute_from_command_line
+	except ImportError as exc:
+		raise ImportError(
+			"Couldn't import Django. Are you sure it's installed and "
+			"available on your PYTHONPATH environment variable? Did you "
+			"forget to activate a virtual environment?"
+		) from exc
+	execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
-    main()
+	main()
