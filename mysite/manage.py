c35bcd5e76e56a0d1fdd4a09034489acdcb00c55
Bayard Rucker <bayardrucker0@gmail.com>
2020-11-19 17:17:51 -0600

    working changes

--- mysite/manage.py
+++ mysite/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-#from mysite import start_redis_server
+from mysite import start_redis_server
 
 def main():
 	"""Run administrative tasks."""
