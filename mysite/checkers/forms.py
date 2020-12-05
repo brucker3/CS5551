bc551ce543aab3e13df3cee33459b7a38aee5055
Sabrina Djeddi <66637870+sabdj2020@users.noreply.github.com>
2020-12-05 14:48:04 -0600

    anonymous

--- mysite/checkers/forms.py
+++ mysite/checkers/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
+
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,7 +10,10 @@ class SignupForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
-class GuestForm(forms.Form):
+class GuestForm(UserChangeForm):
+    def __init__(self, *args, **kwargs):
+        super( GuestForm, self).__init__(*args, **kwargs)
+        del self.fields['password']
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter your username'}))
     class Meta:
         model = User
